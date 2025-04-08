8ea8bde4055d304830b1872bd38fd86904cdcb7c
vitharshanaasreek <vitharshanaasree.k2021ecec@sece.ac.in>
2025-04-08 12:39:28 +0530

    added viewresult

--- EmpJdbcProject/src/com/hexaware/client/EmpClient.java
+++ EmpJdbcProject/src/com/hexaware/client/EmpClient.java
@@ -5,6 +5,12 @@ public class EmpClient {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		display();
+		viewresult();
+	}
+
+	private static void viewresult() {
+		System.out.println("view result");
+		
 	}
 
 	public static void display() {
